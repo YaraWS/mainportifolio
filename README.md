9b92dff66518d0394c0a0f8140e0d8a50bc4648a
YaraWS <39776511+YaraWS@users.noreply.github.com>
2024-01-18 18:13:25 +0000

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mainportifolio
+# Yara Soler - Portfolio🚀
+Welcome to my portfolio! This is a showcase of my work as a passionate front-end developer. You'll find information about me, my projects, and how to get in touch.
---
+++ index.js
@@ -0,0 +1,41 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const form = document.querySelector("#form")
+// let thankYou = document.getElementById("thank-you")
+
+// Hamburger Menu
+hamburger.addEventListener("click", ()=>{
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
+
+
+// function formMessage(fname){
+//   let fname = document.forms["form"]["fname"].value
+//   thankYou.innerHTML = `<div class="user-message"> <span>${fname}</span> thanks for your message<br>
+//   I will get in touch shortly</div>`
+
+// }
+
+// form.addEventListener('submit',function(e){
+//   e.preventDefault();
+//   let fname = document.forms["form"]["fname"].value
+//   document.getElementById("form").style.display="none"
+//   formMessage(fname)
+// })
+
+
+
+
+
+
+
+
+
